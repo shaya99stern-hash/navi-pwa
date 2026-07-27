333df0191b867fae8fb180fe82a60222a1bf0756
shaya99stern-hash <shaya99stern@gmail.com>
2026-07-27 18:01:27 -0400

    Match the Apple touch icon to the Navi starburst

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -5,9 +5,16 @@ export const contentType = "image/png";
 
 export default function AppleIcon() {
   return new ImageResponse(
-    <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center", background: "#0B0D12", color: "#F5F7FB", borderRadius: 40, fontFamily: "Arial", position: "relative" }}>
-      <div style={{ width: 118, height: 118, display: "flex", alignItems: "center", justifyContent: "center", border: "3px solid #262D3E", borderRadius: 32, background: "#11141C", fontSize: 86, fontWeight: 700, letterSpacing: -9, paddingRight: 7 }}>N</div>
-      <div style={{ width: 12, height: 12, position: "absolute", right: 34, bottom: 33, borderRadius: 999, background: "#7A5CFF" }} />
+    <div style={{ width: "100%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center", background: "#100F0D", borderRadius: 40, position: "relative" }}>
+      <div style={{ width: 140, height: 140, display: "flex", alignItems: "center", justifyContent: "center", border: "3px solid #3A332B", borderRadius: 38, background: "#181612" }}>
+        <svg width="102" height="102" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="10" fill="#F8EFE6" />
+          {Array.from({ length: 16 }, (_, index) => (
+            <rect key={index} x="47" y="7" width="6" height="25" rx="3" fill="#F8EFE6" transform={`rotate(${index * 22.5} 50 50)`} />
+          ))}
+        </svg>
+      </div>
+      <div style={{ width: 16, height: 16, position: "absolute", right: 25, bottom: 23, borderRadius: 999, background: "#D77A59", border: "2px solid #100F0D" }} />
     </div>,
     size
   );
